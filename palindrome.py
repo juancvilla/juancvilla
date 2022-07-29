3ce925b4ba2ff9fd9a7dc23124465db5604b7699
Juan Carlos Villarroel Alvarez <juancvilla@gmail.com>
2022-07-29 11:02:43 -0400

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+def is_palindrome(cadena):
+    cadena = cadena.lower()
+    cadena2 = ""
+    for x in range(len(cadena)):
+        cadena2+=cadena[-x-1]
+    return cadena == cadena2
+print(is_palindrome("otto"))
+print("-")
+def is_palindrome2(cadena):
+    cadena = cadena.lower()
+    return cadena == cadena[::-1]
+print(is_palindrome2("ana"))
